978a740994d05f2a6030ba853b9f01b92789eaf6
Pedro Henrique Ronchini <pe-ronchini@hotmail.com>
2024-03-14 21:28:33 -0300

    edit routes

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/showPlanHoliday', [PlanHolidayController::class, 'index']);
-Route::get('/showPlanHoliday/{id}', [PlanHolidayController::class, 'show']);
-Route::post('/storePlanHoliday', [PlanHolidayController::class, 'store']);
-Route::put('/updatePlanHoliday/{id}', [PlanHolidayController::class, 'update']);
-Route::delete('/deletePlanHoliday/{id}', [PlanHolidayController::class, 'destroy']);
+Route::get('/showPlanHoliday', [PlanHolidayController::class, 'index'])->middleware('auth:api');
+Route::get('/showPlanHoliday/{id}', [PlanHolidayController::class, 'show'])->middleware('auth:api');
+Route::post('/storePlanHoliday', [PlanHolidayController::class, 'store'])->middleware('auth:api');
+Route::put('/updatePlanHoliday/{id}', [PlanHolidayController::class, 'update'])->middleware('auth:api');
+Route::delete('/deletePlanHoliday/{id}', [PlanHolidayController::class, 'destroy'])->middleware('auth:api');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
