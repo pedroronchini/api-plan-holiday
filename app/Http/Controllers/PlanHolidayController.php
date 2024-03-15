9d0974abef76620af236ff512a048aad6abed370
Pedro Henrique Ronchini <pe-ronchini@hotmail.com>
2024-03-14 21:10:30 -0300

    create routes plan holiday

---
+++ app/Http/Controllers/PlanHolidayController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PlanHoliday;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PlanHolidayController extends Controller
+{
+    public function index() {
+        $planHolidays = PlanHoliday::all();
+
+        return response()->json($planHolidays, 200);
+    }
+
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'string|required',
+            'description' => 'string|required',
+            'date' => 'required|date_format: Y-m-d',
+            'location' => 'string|required',
+            'participants' => 'string|nullable'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $planHoliday = PlanHoliday::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date,
+            'location' => $request->location,
+            'participants' => $request->participants
+        ]);
+
+        return response()->json($planHoliday, 201);
+    }
+
+    public function show($id) {
+        $planHoliday = PlanHoliday::find($id);
+
+        if (!$planHoliday) {
+            return response()->json(['error' => 'Plan holiday not found'], 404);
+        }
+
+        return response()->json($planHoliday, 200);
+    }
+
+    public function update(Request $request, $id) {
+        $planHoliday = PlanHoliday::find($id);
+
+        if (!$planHoliday) {
+            return response()->json(['error' => 'Plan holiday not found'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'string|nullable',
+            'description' => 'string|nullable',
+            'date' => 'date_format: Y-m-d|nullable',
+            'location' => 'string|nullable',
+            'participants' => 'string|nullable'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $planHoliday->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date,
+            'location' => $request->location,
+            'participants' => $request->participants
+        ]);
+
+        return response()->json($planHoliday, 200);
+    }
+
+    public function destroy($id) {
+        $planHoliday = PlanHoliday::find($id);
+
+        if (!$planHoliday) {
+            return response()->json(['error' => 'Plan holiday not found'], 404);
+        }
+
+        $planHoliday->delete();
+
+        return response()->json(['message' => 'Plan holiday deleted'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PlanHolidayController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/showPlanHoliday', [PlanHolidayController::class, 'index']);
+Route::get('/showPlanHoliday/{id}', [PlanHolidayController::class, 'show']);
+Route::post('/storePlanHoliday', [PlanHolidayController::class, 'store']);
+Route::put('/updatePlanHoliday/{id}', [PlanHolidayController::class, 'update']);
+Route::delete('/deletePlanHoliday/{id}', [PlanHolidayController::class, 'destroy']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
